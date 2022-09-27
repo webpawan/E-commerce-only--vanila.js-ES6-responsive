95f4f858721cbe574dcde25a605de24a4ea69a10
webpawan <pawansaini66477@gmail.com>
2022-09-27 22:13:39 +0530

    category by sorted (it's hard coded)

--- js/index.js
+++ js/index.js
@@ -45,67 +45,255 @@ function decerement(id) {
 localStorage.setItem('data', JSON.stringify(bucket));
 
 }
+
+
+
+
+
+
+
 async function mydata() {
   try {
     let data = await fetch('js/data.json');
     let mydata = await data.json();
-    let apidata = mydata;   
-    
-let genratedata = () => {
-  return (shop.innerHTML = apidata.map((x) => {
-      let { id, name, price, img } = x;
-     
-     let search =bucket.find((x)=>x.itemid == id);
-    //  ${search == undefined ? 0 : search.itmecount}
-    
-
-    return `
-    <div class="mx-auto col-md-6 mb-4 col-lg-4 ">
-    <div class="featured-container p-5">
-      <img
-        src="${img}"
-      class="img-fluid"
-        alt=""
-      />
-      <div class="featured-store-link my-5"
-      ><div
-         class="d-flex justify-content-center align-items-center"
-      >
-        <span
-          class="btn btn-outline-dark mx-1"
-          onclick="decerement(${id})"
-          ><i class="fa-solid fa-minus"></i
-        ></span>
-         <span class="btn btn-outline-dark mx-1" id="${id}">${search == undefined ? 0 : search.itmecount}</span>
-        <span
-          class="btn btn-outline-dark mx-1"
-         onclick="increment(${id})"
-          ><i class="fa-solid fa-plus"></i
-        ></span>
-      </div>
-      </div>
-     </div>
-     <h6 class="text-capitalize text-center my-2">${name}</h6>
-     <h6 class="text-center">
-       <span class="">${price}</span>
-     </h6>
-
-     
-   </div>
-   
-    `
-
-
-
- }).join(""))
-}
-
-genratedata();
-
+    let apidata = mydata;
+    genratedata(apidata);
   } catch (error) {
     console.log("data not fatch");
   }
 }
 mydata();
+let genratedata = (apidata) => {
+
+  let showoutput = document.getElementsByClassName('products-categories-link');
+ 
+ 
+  for (let i = 0; i < showoutput.length; i++){
+  
+    showoutput[i].addEventListener('click', () => {
+      let val = showoutput[i].innerText;
+      
+      if (val == "Football") {
+       
+        let mydata = apidata.filter((ele) => ele.cat == 'ball');
+        return (shop.innerHTML = mydata.map((x) => {
+          let { id, name, price, img } = x;
+          let search = bucket.find((x) => x.itemid == id);
+          return `
+        <div class="mx-auto col-md-6 mb-4 col-lg-4 ">
+        <div class="featured-container p-5">
+          <img
+            src="${img}"
+          class="img-fluid"
+            alt=""
+          />
+          <div class="featured-store-link my-5"
+          ><div
+             class="d-flex justify-content-center align-items-center"
+          >
+            <span
+              class="btn btn-outline-dark mx-1"
+              onclick="decerement(${id})"
+              ><i class="fa-solid fa-minus"></i
+            ></span>
+             <span class="btn btn-outline-dark mx-1" id="${id}">${search == undefined ? 0 : search.itmecount}</span>
+            <span
+              class="btn btn-outline-dark mx-1"
+             onclick="increment(${id})"
+              ><i class="fa-solid fa-plus"></i
+            ></span>
+          </div>
+          </div>
+         </div>
+         <h6 class="text-capitalize text-center my-2">${name}</h6>
+         <h6 class="text-center">
+           <span class="">${price}</span>
+         </h6>
+      
+         
+       </div>
+       
+        `
+      
+        }).join(""))
+      
+
+      } else if (val == "Sport") {
+        let mydata = apidata.filter((ele) => ele.cat == 'spo');
+        return (shop.innerHTML = mydata.map((x) => {
+          let { id, name, price, img } = x;
+          let search = bucket.find((x) => x.itemid == id);
+          return `
+        <div class="mx-auto col-md-6 mb-4 col-lg-4 ">
+        <div class="featured-container p-5">
+          <img
+            src="${img}"
+          class="img-fluid"
+            alt=""
+          />
+          <div class="featured-store-link my-5"
+          ><div
+             class="d-flex justify-content-center align-items-center"
+          >
+            <span
+              class="btn btn-outline-dark mx-1"
+              onclick="decerement(${id})"
+              ><i class="fa-solid fa-minus"></i
+            ></span>
+             <span class="btn btn-outline-dark mx-1" id="${id}">${search == undefined ? 0 : search.itmecount}</span>
+            <span
+              class="btn btn-outline-dark mx-1"
+             onclick="increment(${id})"
+              ><i class="fa-solid fa-plus"></i
+            ></span>
+          </div>
+          </div>
+         </div>
+         <h6 class="text-capitalize text-center my-2">${name}</h6>
+         <h6 class="text-center">
+           <span class="">${price}</span>
+         </h6>
+      
+         
+       </div>
+       
+        `
+      
+        }).join(""))
+      
+
+      } else if (val == 'Sneaker') {
+        let mydata = apidata.filter((ele) => ele.cat == 'sneakers');
+        return (shop.innerHTML = mydata.map((x) => {
+          let { id, name, price, img } = x;
+          let search = bucket.find((x) => x.itemid == id);
+          return `
+        <div class="mx-auto col-md-6 mb-4 col-lg-4 ">
+        <div class="featured-container p-5">
+          <img
+            src="${img}"
+          class="img-fluid"
+            alt=""
+          />
+          <div class="featured-store-link my-5"
+          ><div
+             class="d-flex justify-content-center align-items-center"
+          >
+            <span
+              class="btn btn-outline-dark mx-1"
+              onclick="decerement(${id})"
+              ><i class="fa-solid fa-minus"></i
+            ></span>
+             <span class="btn btn-outline-dark mx-1" id="${id}">${search == undefined ? 0 : search.itmecount}</span>
+            <span
+              class="btn btn-outline-dark mx-1"
+             onclick="increment(${id})"
+              ><i class="fa-solid fa-plus"></i
+            ></span>
+          </div>
+          </div>
+         </div>
+         <h6 class="text-capitalize text-center my-2">${name}</h6>
+         <h6 class="text-center">
+           <span class="">${price}</span>
+         </h6>
+      
+         
+       </div>
+       
+        `
+      
+        }).join(""))
+      
+      } else {
+        return (shop.innerHTML = apidata.map((x) => {
+          let { id, name, price, img } = x;
+         let search =bucket.find((x)=>x.itemid == id);
+        return `
+        <div class="mx-auto col-md-6 mb-4 col-lg-4 ">
+        <div class="featured-container p-5">
+          <img
+            src="${img}"
+          class="img-fluid"
+            alt=""
+          />
+          <div class="featured-store-link my-5"
+          ><div
+             class="d-flex justify-content-center align-items-center"
+          >
+            <span
+              class="btn btn-outline-dark mx-1"
+              onclick="decerement(${id})"
+              ><i class="fa-solid fa-minus"></i
+            ></span>
+             <span class="btn btn-outline-dark mx-1" id="${id}">${search == undefined ? 0 : search.itmecount}</span>
+            <span
+              class="btn btn-outline-dark mx-1"
+             onclick="increment(${id})"
+              ><i class="fa-solid fa-plus"></i
+            ></span>
+          </div>
+          </div>
+         </div>
+         <h6 class="text-capitalize text-center my-2">${name}</h6>
+         <h6 class="text-center">
+           <span class="">${price}</span>
+         </h6>
+      
+         
+       </div>
+       
+        `
+      
+      }).join(""))
+      }
+    })
 
 
+  }
+
+
+
+  return (shop.innerHTML = apidata.map((x) => {
+    let { id, name, price, img } = x;
+   let search =bucket.find((x)=>x.itemid == id);
+  return `
+  <div class="mx-auto col-md-6 mb-4 col-lg-4 ">
+  <div class="featured-container p-5">
+    <img
+      src="${img}"
+    class="img-fluid"
+      alt=""
+    />
+    <div class="featured-store-link my-5"
+    ><div
+       class="d-flex justify-content-center align-items-center"
+    >
+      <span
+        class="btn btn-outline-dark mx-1"
+        onclick="decerement(${id})"
+        ><i class="fa-solid fa-minus"></i
+      ></span>
+       <span class="btn btn-outline-dark mx-1" id="${id}">${search == undefined ? 0 : search.itmecount}</span>
+      <span
+        class="btn btn-outline-dark mx-1"
+       onclick="increment(${id})"
+        ><i class="fa-solid fa-plus"></i
+      ></span>
+    </div>
+    </div>
+   </div>
+   <h6 class="text-capitalize text-center my-2">${name}</h6>
+   <h6 class="text-center">
+     <span class="">${price}</span>
+   </h6>
+
+   
+ </div>
+ 
+  `
+
+}).join(""))
+
+
+}
